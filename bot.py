95b096f82de9222cd6b997babadcc7f06a289de4
snayp25 <muradsultanov466@gmail.com>
2024-07-27 16:49:17 +0400

    ok

--- bot.py
+++ bot.py
@@ -1,29 +1,49 @@
 import telebot
 from main import Text2ImageAPI
-count = 0 
+import logging
+import time
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
 API_TOKEN = '6897585982:AAGjL-bQPF5zrsdPbr17N8MNneJbTUdLohc'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+def start(update: Update, context: CallbackContext) -> None:
+    # Показываем статус "печатает"
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action="typing")
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+Привет, я EchoBot.
+Я здесь для того, чтобы повторять за вами ваши добрые слова. Просто скажите что-нибудь приятное, и я скажу вам то же самое!\
 """)
-
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, """Я бот для генерации картинок. Напишите то что хотите сгенерировать.
+""")
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
+    bot.send_chat_action(chat_id=message.chat.id, action="typing")
+    import time
+    time.sleep(5)
+    # bot.send_message(message.chat.id, "typing")
     bot.reply_to(message, "Ваш заказ принят, ожидайте..")
     api = Text2ImageAPI('https://api-key.fusionbrain.ai/', '9987ED3717CA963CDB314E972CBF56AD', 'C380681D77C9EB0A8BE5A812615A3668')
     model_id = api.get_model()
     uuid = api.generate(message.text, model_id)
     images = api.check_generation(uuid)
-   
+    
 
   
     filename = f"image_{message.chat.id}.jpg"
@@ -31,9 +51,9 @@ def echo_message(message):
 
     image = open (filename, 'rb')
     bot.send_photo(message.chat.id, photo=image)
-    count =+ 1 
 
-    if count == 2:
-        bot.reply_to(message, "-")
+
+    
 
 bot.infinity_polling()
+
